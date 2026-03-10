6d19b71477b714ced72769b8d2876ace77eb7c06
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2026-03-09 23:57:43 -0700

    Add basic nova

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from amazon-nova-hackathon!")
+from openai import OpenAI
 
+with open("key.secret") as file:
+    KEY = file.read().rstrip()
 
-if __name__ == "__main__":
-    main()
+client = OpenAI(api_key=KEY, base_url="https://api.nova.amazon.com/v1")
+
+response = client.chat.completions.create(
+    model="nova-2-lite-v1",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello! How are you?"},
+    ],
+    stream=False,
+)
+
+print(response.choices[0].message.content)
