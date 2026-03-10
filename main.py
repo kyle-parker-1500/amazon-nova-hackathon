463123d562666de2a9614d779f1a5bd459d90bb7
kyle-parker-1500 <kylerparker1500@gmail.com>
2026-03-10 00:23:16 -0700

    finished first version of textin/out

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hey Kyle")
+from openai import OpenAI
+
+try:
+    with open("./key.secret", 'r') as file:
+        api_key = file.read().strip()
+except FileNotFoundError:
+    print("File not found.")
+
+client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.nova.amazon.com/v1"
+)
+
+response = client.chat.completions.create(
+    model="nova-2-lite-v1",
+    messages=[{
+            "role": "system",
+            "content": "You are a helpful assistant"
+        },
+        {
+            "role": "user",
+            "content": "Hello! How are you?"
+        },
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
