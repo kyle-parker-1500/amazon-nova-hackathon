c9427681534a5f94ba78a71c26c94fef26136d3e
kyle-parker-1500 <kylerparker1500@gmail.com>
2026-03-13 19:30:59 -0700

    modified variable input to api

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ client = OpenAI(
     base_url="https://api.nova.amazon.com/v1"
 )
 
+define_assistant = input("Define how your assistant will behave: ")
+enter_query = input("Enter a query: ")
+
 response = client.chat.completions.create(
     model="nova-2-lite-v1",
     messages=[{
             "role": "system",
-            "content": "You are a helpful assistant"
-        },
+            "content": define_assistant
+    },
         {
             "role": "user",
-            "content": "Hello! How are you?"
+            "content": enter_query
         },
     ],
     stream=False
